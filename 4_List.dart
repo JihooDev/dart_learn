ae2c3b06584fe59faf47acc74e38f4ce13e25743
jihooDev <wlgn829@kakao.com>
2023-10-30 09:05:40 +0900

    feat: List

---
+++ 4_List.dart
@@ -0,0 +1,18 @@
+void main() {
+  // List
+  List<String> korean = ['가', '나', '다', '라'];
+
+  // 접근
+  print(korean[0]); // 가
+
+  // length
+  print(korean.length); // 4
+
+  // 값 추가 (add)
+  korean.add('마');
+  print(korean); // [가, 나, 다, 라, 마]
+
+  // 값 삭제 (remove)
+  korean.remove('가');
+  print(korean); // [나, 다, 라, 마]
+}
